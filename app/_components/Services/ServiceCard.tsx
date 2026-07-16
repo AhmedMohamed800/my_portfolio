278394d97ac8cc7e51a4f45cd3a795baf552e4b8
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2026-07-16 16:33:46 +0300

    fix: fix ServiceCard for small devices

--- app/_components/Services/ServiceCard.tsx
+++ app/_components/Services/ServiceCard.tsx
@@ -13,9 +13,9 @@ export default function ServiceCard({
     <article className="flex flex-col gap-4 w-112.5 lg:w-160">
       <div
         style={{ backgroundImage: `url('/${img}')` }}
-        className="h-100 w-full bg-cover bg-no-repeat rounded-sm "
+        className="h-100 w-full bg-cover bg-no-repeat rounded-sm bg-center"
       ></div>
-      <div className="flex justify-between items-center gap-4 text-white">
+      <div className="flex flex-col md:flex-row justify-between md:items-center gap-2 md:gap-4 text-white">
         <h2 className="text-[clamp(1.5rem,2.8vw,2.25rem)] font-bold leading-[1.35] uppercase">
           {title}
         </h2>
