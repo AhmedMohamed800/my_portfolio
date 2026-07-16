eaa7e548424695b028b9b7023d8a9f103f933786
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2026-07-16 16:58:19 +0300

    feat: create skills section

---
+++ app/_components/Skills/SkillsSection.tsx
@@ -0,0 +1,9 @@
+export default function SkillsSection() {
+  return (
+    <section className="bg-black ">
+      <div className="section-container container-padding text-white pt-12 pb-12 lg:pt-16 lg:pb-16 min-h-dvh">
+        <h1 className="text-5xl font-bold  uppercase">sKILLS & tools</h1>
+      </div>
+    </section>
+  );
+}
